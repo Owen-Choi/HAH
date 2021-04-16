6f94e67ada5f5e304e8bdcbb378f32d50e2de8dd
Owen-Choi <oldstyle482@gmail.com>
2021-04-16 16:00:54 +0900

    플레이어 추격 AI 구현 시도, 좀비 AI 개편에 따른 일부 애니메이션 수정 Attempt to implement player-chasing AI and modify some animations due to zombie AI reorganization

--- Evolver/Assets/Scripts/Zombie_AI.cs
+++ Evolver/Assets/Scripts/Zombie_AI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Zombie_AI : MonoBehaviour
 {
+    public Transform Player;
+    public float radius; float degree;
     public int hMove;
     public int vMove;
     int term;
@@ -11,21 +13,66 @@ public class Zombie_AI : MonoBehaviour
     Rigidbody2D rigid;
     Animator anim;
     public float MoveSpeed;
-    
+
     void Awake()
     {
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
-        MoveAI();
+        //MoveAI();
     }
 
     void Update()
     {
 
-        if (hMove > 0 || hMove < 0)
-            isHorizonMove = true;
-        else if (hMove == 0)
-            isHorizonMove = false;
+        RaycastHit2D circle = Physics2D.CircleCast(transform.position, radius, Vector2.up, radius, LayerMask.GetMask("Player"));
+        Debug.Log(circle);
+
+        if (circle.collider.tag == "Player")
+        {
+            anim.SetFloat("HorizonValue", (Player.position.x - this.transform.position.x));
+            anim.SetFloat("VerticalValue", (Player.position.y - this.transform.position.y));
+
+           
+            if (GetAngle() > 0 && GetAngle() <= 1.5f)
+            {
+                anim.SetBool("isChange", true);
+                anim.SetBool("isLeft", false);
+                anim.SetBool("isRight", true);
+                anim.SetBool("isBack", true);
+                
+            }
+
+            if (GetAngle() >= 1.5f && GetAngle() < 3.0f)
+            {
+                anim.SetBool("isChange", true);
+                anim.SetBool("isRight", false);
+                anim.SetBool("isLeft", true);
+                anim.SetBool("isBack", true);
+            }
+
+            if (GetAngle() > -3.0f && GetAngle() <= -1.5f)
+            {
+                anim.SetBool("isChange", true);
+                anim.SetBool("isRight", false);
+                anim.SetBool("isLeft", true);
+                anim.SetBool("isBack", false);
+            }
+
+            if (GetAngle() > -1.5f && GetAngle() <= 0f)
+            {
+                anim.SetBool("isChange", true);
+                anim.SetBool("isLeft", false);
+                anim.SetBool("isRight", true);
+                anim.SetBool("isBack", false);
+            }
+            this.transform.position = Vector3.MoveTowards(this.transform.position, Player.position, MoveSpeed * Time.deltaTime);
+        }
+       
+
+            if (hMove > 0 || hMove < 0)
+                isHorizonMove = true;
+            else if (hMove == 0)
+                isHorizonMove = false;
 
         if (anim.GetInteger("HorizonValue") != hMove)
         {
@@ -39,9 +86,10 @@ public class Zombie_AI : MonoBehaviour
         }
         else
         {
-            anim.SetBool("isChange", false);
+            //anim.SetBool("isChange", false);
         }
 
+       
     }
 
     void FixedUpdate()
@@ -53,14 +101,33 @@ public class Zombie_AI : MonoBehaviour
     void MoveAI()
     {
 
-        hMove = Random.Range(-1, 2);
-        if (hMove == 0)
-            vMove = Random.Range(-1, 2);
-        term = Random.Range(1, 3);
+        if(Random.Range(0,100) < 30)
+        {
+            hMove = 0;
+            vMove = 0;
+            anim.SetBool("isIdle", true);
+            Invoke("MoveAI", 2f);
+        }
+        else
+        {
+            if(Random.Range(0,2) == 0) {
+                ;
+            }
+        }
         Invoke("MoveAI", term);
+
+    } 
+
+        float GetAngle()
+        {
+            Vector3 difference = Player.position - transform.position;
+
+            difference.Normalize();
+
+
+            return degree = Mathf.Atan2(difference.y, difference.x);
+
+        }
 
     }
 
-   
-
-}
