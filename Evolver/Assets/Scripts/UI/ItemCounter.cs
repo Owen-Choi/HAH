08eaa85becc471cdbaf2acea9b6954244eb212d0
Owen-Choi <oldstyle482@gmail.com>
2021-04-19 23:43:57 +0900

    인벤토리 시스템과 UI연동하여 플레이어 화면의 오른쪽에 아이템 리스트 표시 및 관련 코드 추가 Item list add to main player's right side by connecting inventory system and UI, and add some code about it

--- Evolver/Assets/Scripts/UI/ItemCounter.cs
+++ Evolver/Assets/Scripts/UI/ItemCounter.cs
@@ -5,9 +5,15 @@ using UnityEngine.UI;
 public class ItemCounter : MonoBehaviour
 {
     public Text MutantSampleCount;
+    public Text BandageCount;
+    public Text MedikitCount;
+    public Text StaminaPotionCount;
     public GameObject BackPack;
     void Update()
     {
         MutantSampleCount.text = BackPack.GetComponent<BackPack>().GetItemCount("MutantSample").ToString();
+        BandageCount.text = BackPack.GetComponent<BackPack>().GetItemCount("Bandage").ToString();
+        MedikitCount.text = BackPack.GetComponent<BackPack>().GetItemCount("Medikit").ToString();
+        StaminaPotionCount.text = BackPack.GetComponent<BackPack>().GetItemCount("StaminaPotion").ToString();
     }
 }
