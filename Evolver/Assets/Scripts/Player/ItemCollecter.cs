a172dfbf7156fbe4cfd0e6e26817ab2a6e305c3a
Owen-Choi <oldstyle482@gmail.com>
2021-05-02 15:46:34 +0900

    씬의 단일화에 따른 최적화 작업 및 코드 수정

--- Evolver/Assets/Scripts/Player/ItemCollecter.cs
+++ Evolver/Assets/Scripts/Player/ItemCollecter.cs
@@ -5,15 +5,16 @@ using UnityEngine.SceneManagement;
 public class ItemCollecter : MonoBehaviour
 {
     int tempCount;
-
+    public GameObject BasicUI;
+    public GameObject BackPack;
     private void Start()
     {
         if (SceneManager.GetActiveScene().name != "Shelter")
         {
-            GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().MutantSampleCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("MutantSample").ToString();
-            GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().BandageCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("Bandage").ToString();
-            GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().MedikitCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("Medikit").ToString();
-            GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().StaminaPotionCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("StaminaPotion").ToString();
+            BasicUI.GetComponent<ItemCounterForMapping>().MutantSampleCount.text = BackPack.GetComponent<BackPack>().GetItemCount("MutantSample").ToString();
+            BasicUI.GetComponent<ItemCounterForMapping>().BandageCount.text = BackPack.GetComponent<BackPack>().GetItemCount("Bandage").ToString();
+            BasicUI.GetComponent<ItemCounterForMapping>().MedikitCount.text = BackPack.GetComponent<BackPack>().GetItemCount("Medikit").ToString();
+            BasicUI.GetComponent<ItemCounterForMapping>().StaminaPotionCount.text = BackPack.GetComponent<BackPack>().GetItemCount("StaminaPotion").ToString();
         }
     }
 
@@ -25,26 +26,26 @@ public class ItemCollecter : MonoBehaviour
             if (other.gameObject.layer == LayerMask.NameToLayer("MutantSample"))
             {
                 tempCount = Random.Range(1, 4);
-                GameObject.Find("BackPack").GetComponent<BackPack>().AddItem("MutantSample", tempCount);
-                GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().MutantSampleCount.text = (int.Parse(GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().MutantSampleCount.text) + tempCount).ToString();
+                BackPack.GetComponent<BackPack>().AddItem("MutantSample", tempCount);
+                BasicUI.GetComponent<ItemCounterForMapping>().MutantSampleCount.text = (int.Parse(BasicUI.GetComponent<ItemCounterForMapping>().MutantSampleCount.text) + tempCount).ToString();
             }
 
             if (other.gameObject.layer == LayerMask.NameToLayer("Medikit"))
             {
-                GameObject.Find("BackPack").GetComponent<BackPack>().AddItem("Medikit", 1);
-                GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().MedikitCount.text = (int.Parse(GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().MedikitCount.text) + 1).ToString();
+                BackPack.GetComponent<BackPack>().AddItem("Medikit", 1);
+                BasicUI.GetComponent<ItemCounterForMapping>().MedikitCount.text = (int.Parse(BasicUI.GetComponent<ItemCounterForMapping>().MedikitCount.text) + 1).ToString();
             }
 
             if (other.gameObject.layer == LayerMask.NameToLayer("Bandage"))
             {
-                GameObject.Find("BackPack").GetComponent<BackPack>().AddItem("Bandage", 1);
-                GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().BandageCount.text = (int.Parse(GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().BandageCount.text) + 1).ToString();
+                BackPack.GetComponent<BackPack>().AddItem("Bandage", 1);
+                BasicUI.GetComponent<ItemCounterForMapping>().BandageCount.text = (int.Parse(BasicUI.GetComponent<ItemCounterForMapping>().BandageCount.text) + 1).ToString();
             }
 
             if (other.gameObject.layer == LayerMask.NameToLayer("StaminaPotion"))
             {
-                GameObject.Find("BackPack").GetComponent<BackPack>().AddItem("StaminaPotion", 1);
-                GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().StaminaPotionCount.text = (int.Parse(GameObject.Find("BasicUI").GetComponent<ItemCounterForMapping>().StaminaPotionCount.text) + 1).ToString();
+                BackPack.GetComponent<BackPack>().AddItem("StaminaPotion", 1);
+                BasicUI.GetComponent<ItemCounterForMapping>().StaminaPotionCount.text = (int.Parse(BasicUI.GetComponent<ItemCounterForMapping>().StaminaPotionCount.text) + 1).ToString();
             }
         }
     }
--- Evolver/Assets/Scripts/UI/ItemCounter.cs
+++ Evolver/Assets/Scripts/UI/ItemCounter.cs
@@ -10,16 +10,17 @@ public class ItemCounter : MonoBehaviour
     public Text StaminaPotionCount;
     public Text FoodCount;
     public Text WaterCount;
+    public GameObject BackPack;
     //public GameObject BackPack;
-
-    void Start()
+    
+    void Update()
     {
         // # Update 함수 안에서 Find는 절대로 써선 안된다. Awake나 Start에서 한번 Find로 찾아서 UI에 표시해주고 그 뒤로는 변동이 생길 때 마다 해당 오브젝트에서 변경을 해야할 것 같다.
-        MutantSampleCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("MutantSample").ToString();
-        BandageCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("Bandage").ToString();
-        MedikitCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("Medikit").ToString();
-        StaminaPotionCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("StaminaPotion").ToString();
-        FoodCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("Food").ToString();
-        WaterCount.text = GameObject.Find("BackPack").GetComponent<BackPack>().GetItemCount("Water").ToString();
+        MutantSampleCount.text = BackPack.GetComponent<BackPack>().GetItemCount("MutantSample").ToString();
+        BandageCount.text = BackPack.GetComponent<BackPack>().GetItemCount("Bandage").ToString();
+        MedikitCount.text = BackPack.GetComponent<BackPack>().GetItemCount("Medikit").ToString();
+        StaminaPotionCount.text = BackPack.GetComponent<BackPack>().GetItemCount("StaminaPotion").ToString();
+        FoodCount.text = BackPack.GetComponent<BackPack>().GetItemCount("Food").ToString();
+        WaterCount.text = BackPack.GetComponent<BackPack>().GetItemCount("Water").ToString();
     }
 }
