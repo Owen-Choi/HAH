b4948a712be9d9fdb71883a91dcb8a52848a301b
Owen-Choi <oldstyle482@gmail.com>
2021-05-14 10:41:12 +0900

    신체부위 가슴 스킬 업데이트 Body part chest skill update

---
+++ Evolver/Assets/Scripts/Skills/Physical/Chest/Growing.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Growing : Physical_Manager
+{
+    int PlayerLevel;
+    private void Awake()
+    {
+        PlayerLevel = Player_Stat.instance.Physical_Level;
+    }
+
+    void Update()
+    {
+        if (this.Selected)
+        {
+            this.Selected = false;
+            if(PlayerLevel < Player_Stat.instance.Physical_Level)       //참조 개념이 내가 생각하는 것과 다르다면 동작하지 않을 코드이다.
+            {
+                PlayerLevel = Player_Stat.instance.Physical_Level;
+                Player_Stat.instance.DefaultHealthMax += 5f;           
+            }
+        }
+    }
+}
---
+++ Evolver/Assets/Scripts/Skills/Physical/Chest/StaminaRecoveryIncrease.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaminaRecoveryIncrease :  Physical_Manager
+{
+    void Update()
+    {
+        if (this.Selected)
+        {
+            this.Selected = false;
+            Player_Stat.instance.Stamina_recovery_speed += 1f;          //수치 조정 가능성 있음.
+        }
+    }
+}
